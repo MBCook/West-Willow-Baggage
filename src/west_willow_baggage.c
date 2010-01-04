3b17d9c67be555e3fc2a501cd39b7b90088029de
Michael Cook <foobarsoft@foobarsoft.com>
2010-01-04 18:09:08 +0000

    Adding filtering to the program. Things are perfect for the non-evil version, except for checking the correctness of bag records.

--- src/west_willow_baggage.c
+++ src/west_willow_baggage.c
@@ -7,20 +7,37 @@
  ============================================================================
  */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "bag_carousel.h"
 #include "bag_record.h"
 
-void print_carousel(bag_carousel *carousel, char* buffer, int buffer_size);
+// Forward definitions
 
-int main(void) {
+void print_carousel(bag_carousel *carousel, char* buffer, int buffer_size,
+							char *luggage, char* flight, char *source, char *destination);
+bool filter_bag(bag_record *bag, char *luggage, char* flight, char *source, char *destination);
+
+// Meat of the file
+
+int main(int argc, char** argv) {
+	// OK, first let's make sure our command line arguments are sane
+
+	if (argc != 5) {
+		printf("Usage: %s LUGGAGE_ID FLIGHT_ID DEPARTING ARRIVING\n", argv[0]);
+		printf("\tEach argument is a search string for that piece of information\n");
+		printf("\tSubstituting a hyphen for a search string acts as a wildcard\n");
+
+		exit(EXIT_FAILURE);
+	}
+
+	// OK, our arguments are sane, let's move on and load in all the luggage data
+
 	char buffer[1024];
 
 	memset(&buffer, '\0', 1024);
-
-	printf("Please enter a bag record per line, bank or EOF to end:\n");
 
 	bag_carousel *carousel = NULL;
 
@@ -59,31 +76,66 @@ int main(void) {
 		memset(buffer, '\0', 1024);
 	}
 
-	// OK, we're done, we'll need to dump bag history
+	// OK, we're done, we'll need to dump bag history, based on our filter
 
-	printf("The most recent information is:\n");
-
-	print_carousel(carousel, buffer, 1024);
+	print_carousel(carousel, buffer, 1024, argv[1], argv[2], argv[3], argv[4]);
 
 	cleanup_carousel(carousel);
 
 	exit(EXIT_SUCCESS);
 }
 
-void print_carousel(bag_carousel *carousel, char* buffer, int buffer_size) {
+void print_carousel(bag_carousel *carousel, char* buffer, int buffer_size,
+									char *luggage, char* flight, char *source, char *destination) {
 	if (carousel->left != NULL)
-		print_carousel(carousel->left, buffer, buffer_size);
+		print_carousel(carousel->left, buffer, buffer_size, luggage, flight, source, destination);
 
 	bag_bin *bin = carousel->bin;
 
 	while (bin != NULL) {
-		print_bag_record(bin->bag, buffer, 1024);
+		if (filter_bag(bin->bag, luggage, flight, source, destination)) {
+			print_bag_record(bin->bag, buffer, 1024);
 
-		printf("%s\n", buffer);
+			printf("%s\n", buffer);
+
+			memset(buffer, '\0', 1024);
+		}
 
 		bin = bin->next;
 	}
 
 	if (carousel->right != NULL)
-		print_carousel(carousel->right, buffer, buffer_size);
+		print_carousel(carousel->right, buffer, buffer_size, luggage, flight, source, destination);
+}
+
+bool filter_bag(bag_record *bag, char *luggage, char* flight, char *source, char *destination) {
+	if (strncmp("-", luggage, 1) != 0) {
+		// Filter on luggage ID
+
+		if (strncmp(bag->luggage_id, luggage, 8) != 0)
+			return false;
+	}
+
+	if (strncmp("-", flight, 1) != 0) {
+		// Filter on flight ID
+
+		if (strncmp(bag->flight_id, flight, 6) != 0)
+			return false;
+	}
+
+	if (strncmp("-", source, 1) != 0) {
+		// Filter on departing airport ID
+
+		if (strncmp(bag->source, source, 3) != 0)
+			return false;
+	}
+
+	if (strncmp("-", destination, 1) != 0) {
+		// Filter on destination airport ID
+
+		if (strncmp(bag->destination, destination, 3) != 0)
+			return false;
+	}
+
+	return true;
 }
